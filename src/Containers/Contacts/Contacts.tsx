032a001ab9d9d080bfa035e54e0ab6187a25e071
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-10-21 14:01:36 +0200

    Created basic page layout for contacts with a form. Will update styles further

--- src/Containers/Contacts/Contacts.tsx
+++ src/Containers/Contacts/Contacts.tsx
@@ -1,9 +1,34 @@
+import { Box, Button, Container, TextField, Typography } from '@mui/material';
 
 
 const Contacts = () => {
   return (
     <div>
-      Contacts
+      <Container>
+        <Typography variant="h4" component="h1" gutterBottom align="center">
+          Contact Us
+        </Typography>
+
+        <Typography variant="body1" align="center">
+          If you have any questions or inquiries, feel free to reach out to us using the form below.
+        </Typography>
+
+        <Box component="form">
+          <TextField
+            label="name"
+          />
+          <TextField
+            label="Email"
+            type="email"
+          />
+          <TextField
+            label="Message"
+          />
+          <Button>
+            Send message
+          </Button>
+        </Box>
+      </Container>
     </div>
   );
 };
